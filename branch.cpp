23d294b60e568f61464fd16f05675df9f24ca079
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2022-12-13 20:27:47 +0600

    Create branch.cpp

---
+++ branch.cpp
@@ -0,0 +1,46 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+int main()
+{
+    Boost;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<int> arr(n + 1, 1);
+        while (m--)
+        {
+
+        }
+        long long out = 0;
+        for (ll i = 1; i <= n; i++)
+        {
+            arr[i] = max(arr[i], arr[i - 1]);
+            out += (long long)(i - arr[i] + 1);
+        }
+        cout << out << "\n";
+    }
+
+    return 0;
+}
