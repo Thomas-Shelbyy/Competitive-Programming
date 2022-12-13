45595a1b1a02b10bbc91c459c005482d8d194772
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2022-12-13 20:19:55 +0600

    Create sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,66 @@
+// /*
+
+// string author;
+// author = Hafiz_Sakib;
+
+// */
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define endl "\n"
+// #define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+// #define Boost                         \
+//     ios_base::sync_with_stdio(false); \
+//     cin.tie(NULL);                    \
+//     cout.tie(NULL)
+
+// #define debug(x) cerr << x << endl;
+// #define here fprintf(stderr, "====I am Here====\n");
+
+// int main()
+// {
+//     Boost;
+
+//     int t = 1;
+//     cin >> t;
+//     while (t--)
+//     {
+//         string s;
+//         cin >> s;
+//         int ans = (s.size() - 1) * 9;
+//         ans += (s[0] - '0');
+//         cout << ans << "\n";
+//     }
+
+//     return 0;
+// }
+
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+int main()
+{
+    Boost;
+
+    string s = "sakib";
+    int t = s.find("ak");
+    cout << t << endl;
+
+    return 0;
+}
