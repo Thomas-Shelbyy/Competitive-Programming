a5de4a92f0cd3ae87d475b202f07a68bafcc36cd
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2022-12-13 20:26:32 +0600

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,36 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int ans = (s.size() - 1) * 9;
+        ans += (s[0] - '0');
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
